153eb4e5b196b9e1a602a48d86ca683c975aa457
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-08-08 16:32:24 +0200

    test: Add unit test for Hello function in main_test.go

---
+++ hello/main_test.go
@@ -0,0 +1,42 @@
+package main_test
+
+import (
+	"context"
+	"flag"
+	"os"
+	"testing"
+
+	"dagger.io/dagger"
+	"github.com/stretchr/testify/require"
+)
+
+var c *dagger.Client
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	ctx := context.Background()
+
+	c, _ = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	defer c.Close()
+
+	code := m.Run()
+	defer c.Close()
+	os.Exit(code)
+}
+
+func Test_Hello(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	t.Run("Test_hellocontainer_function", func(t *testing.T) {
+		t.Parallel()
+		stringArg := "Hello, Daggerverse!"
+		out, err := c.Container().
+			From("busybox:uclibc").
+			WithExec([]string{"echo", stringArg}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		require.Equal(t, "Hello, Daggerverse!\n", out)
+	})
+}
