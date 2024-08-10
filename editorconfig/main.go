577b826acf94f50caef7e6866a82f05fe01aeb50
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-08-10 16:41:58 +0200

    feat: Add Dagger module files and configuration for Editorconfig

---
+++ editorconfig/main.go
@@ -0,0 +1,46 @@
+// A generated module for Editorconfig functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+package main
+
+import (
+	"dagger/editorconfig/internal/dagger"
+)
+
+// Editorconfig is a module for checking editorconfig files.
+type Editorconfig struct{}
+
+// Check runs the editorconfig-checker command.
+func (m *Editorconfig) Check(
+	// source is an optional argument that specifies a directory.
+	source *dagger.Directory,
+) *dagger.Container {
+	return ec().
+		WithMountedDirectory("/tmp", source).
+		WithWorkdir("/tmp").
+		WithExec([]string{"/editorconfig-checker"})
+}
+
+// ec returns a container with the editorconfig-checker binary installed.
+func ec() *dagger.Container {
+	install := dag.
+		Container().
+		From("golang:alpine").
+		WithExec([]string{
+			"go", "install",
+			"github.com/editorconfig-checker/editorconfig-checker/cmd/editorconfig-checker@latest",
+		})
+
+	return dag.Container().
+		WithFile("/", install.File("/go/bin/editorconfig-checker"))
+}
