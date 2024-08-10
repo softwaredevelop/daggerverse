fe167eac160d30a0f804700155d655657a2250cb
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-08-10 17:02:42 +0200

    test: Add unit test for Editorconfig function in main_test.go

---
+++ editorconfig/main_test.go
@@ -0,0 +1,78 @@
+package main_test
+
+import (
+	"context"
+	"flag"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"dagger.io/dagger"
+	"github.com/stretchr/testify/require"
+)
+
+var c *dagger.Client
+
+func TestMain(m *testing.M) {
+	flag.Parse()
+
+	ctx := context.Background()
+
+	c, _ = dagger.Connect(ctx, dagger.WithLogOutput(os.Stderr))
+	defer c.Close()
+
+	code := m.Run()
+	defer c.Close()
+	os.Exit(code)
+}
+
+func Test_Editorconfig(t *testing.T) {
+	t.Parallel()
+	ctx := context.Background()
+
+	t.Run("Test_mounted_host_directory_without_git_directory", func(t *testing.T) {
+		t.Parallel()
+		container := ec(c)
+		require.NotNil(t, container)
+
+		dir, err := os.Getwd()
+		require.NoError(t, err)
+		require.NotEmpty(t, dir)
+
+		out, err := container.
+			WithMountedDirectory("/tmp", c.Host().Directory(filepath.Join(dir, ".."), dagger.HostDirectoryOpts{
+				Exclude: []string{".git"},
+			})).
+			WithWorkdir("/tmp").
+			WithExec([]string{"/editorconfig-checker", "-dry-run"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, out)
+	})
+	t.Run("Test_mounted_host_directory", func(t *testing.T) {
+		t.Parallel()
+		container := ec(c)
+		require.NotNil(t, container)
+
+		out, err := container.
+			WithMountedDirectory("/tmp", c.Host().Directory(".")).
+			WithWorkdir("/tmp").
+			WithExec([]string{"/editorconfig-checker", "-dry-run"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		require.NotNil(t, out)
+	})
+}
+
+func ec(c *dagger.Client) *dagger.Container {
+	install := c.
+		Container().
+		From("golang:alpine").
+		WithExec([]string{
+			"go", "install",
+			"github.com/editorconfig-checker/editorconfig-checker/cmd/editorconfig-checker@latest",
+		})
+
+	return c.Container().
+		WithFile("/", install.File("/go/bin/editorconfig-checker"))
+}
