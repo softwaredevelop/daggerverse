a45c2370f8d07775ef10d8196c0fe2dd696ca957
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-08-12 19:32:06 +0200

    test: Add Shellcheck module test files and configuration

---
+++ shellcheck/test/main.go
@@ -0,0 +1,51 @@
+// A generated module for Shellchecktest functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+package main
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sourcegraph/conc/pool"
+)
+
+// Shellchecktest is a module for testing shell scripts.
+type Shellchecktest struct{}
+
+// All runs all tests.
+func (m *Shellchecktest) All(ctx context.Context) error {
+	p := pool.New().WithErrors().WithContext(ctx)
+
+	p.Go(m.CheckDirectory)
+
+	return p.Wait()
+}
+
+// CheckDirectory runs a test on a directory.
+func (m *Shellchecktest) CheckDirectory(ctx context.Context) error {
+
+	dir := dag.CurrentModule().Source().Directory("./testdata")
+	_, err := dag.Shellcheck().Check(dir).Stdout(ctx)
+
+	if err != nil {
+		errorIDs := []string{"SC2283", "SC2154", "SC2086"}
+		for _, id := range errorIDs {
+			if !strings.Contains(err.Error(), id) {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
