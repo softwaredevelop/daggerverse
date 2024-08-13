6c7c1ecbf02eba7ced76262329ee880fbb9946a3
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-08-13 21:02:16 +0200

    test: Add test files and configurations for Revive linter

---
+++ revive/test/main.go
@@ -0,0 +1,48 @@
+// A generated module for Revivetest functions
+//
+// This module has been generated via dagger init and serves as a reference to
+// basic module structure as you get started with Dagger.
+//
+// Two functions have been pre-created. You can modify, delete, or add to them,
+// as needed. They demonstrate usage of arguments and return types using simple
+// echo and grep commands. The functions can be called from the dagger CLI or
+// from one of the SDKs.
+//
+// The first line in this comment block is a short description line and the
+// rest is a long description with more detail on the module's purpose or usage,
+// if appropriate. All modules should have a short description.
+package main
+
+import (
+	"context"
+	"strings"
+
+	"github.com/sourcegraph/conc/pool"
+)
+
+// Revivetest is a Dagger module that provides functions for running Revive linter
+type Revivetest struct{}
+
+// All runs all tests.
+func (m *Revivetest) All(ctx context.Context) error {
+	p := pool.New().WithErrors().WithContext(ctx)
+
+	p.Go(m.Check)
+
+	return p.Wait()
+}
+
+// Check runs the revive command.
+func (m *Revivetest) Check(ctx context.Context) error {
+
+	dir := dag.CurrentModule().Source().Directory("./testdata")
+	_, err := dag.Revive().Check(dir).Stdout(ctx)
+
+	if err != nil {
+		if !strings.Contains(err.Error(), "don't use underscores in Go names") {
+			return err
+		}
+	}
+
+	return nil
+}
