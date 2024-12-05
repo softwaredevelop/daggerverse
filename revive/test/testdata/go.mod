6c1b4b90d72d987330a9f4f7db022f1eceb61df6
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-12-05 21:18:48 +0100

    test: Add revive testdata files

---
+++ revive/test/testdata/go.mod
@@ -0,0 +1,3 @@
+module dagger/revive/test/testdata
+
+go 1.22.5
---
+++ revive/test/testdata/t.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+
+	var unusedVar string
+
+	var my_var int = 10
+	fmt.Println(my_var)
+}
