60e3ec9dad7f4b9f93e3c31f95c859c467fa70ce
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-12-14 15:27:39 +0100

    test: Update main_test.go

--- quarto/main_test.go
+++ quarto/main_test.go
@@ -25,13 +25,38 @@ func TestMain(m *testing.M) {
 	os.Exit(code)
 }
 
-func Test_Quatro(t *testing.T) {
+func Test_Quarto(t *testing.T) {
 	t.Parallel()
 	ctx := context.Background()
 
-	t.Run("Test_quatro_version", func(t *testing.T) {
+	t.Run("Test_quarto_full_render", func(t *testing.T) {
 		t.Parallel()
-		container := base(nil)
+		container := base("ghcr.io/quarto-dev/quarto-full")
+		require.NotNil(t, container)
+
+		out, err := container.
+			WithMountedDirectory("/tmp", c.Host().Directory("./test/testdata/")).
+			WithWorkdir("/tmp").
+			WithExec([]string{"quarto", "render"}).
+			WithExec([]string{"ls", "-1", "_book"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		require.Regexp(t, `\.pdf\s*$`, out)
+	})
+	t.Run("Test_quarto_full_version", func(t *testing.T) {
+		t.Parallel()
+		container := base("ghcr.io/quarto-dev/quarto-full")
+		require.NotNil(t, container)
+
+		out, err := container.
+			WithExec([]string{"quarto", "--version"}).
+			Stdout(ctx)
+		require.NoError(t, err)
+		require.Regexp(t, `\d+\.\d+\.\d+`, out)
+	})
+	t.Run("Test_quarto_version", func(t *testing.T) {
+		t.Parallel()
+		container := base("")
 		require.NotNil(t, container)
 
 		out, err := container.
@@ -43,14 +68,14 @@ func Test_Quatro(t *testing.T) {
 }
 
 func base(
-	image *string,
+	image string,
 ) *dagger.Container {
 
 	defaultImageRepository := "ghcr.io/quarto-dev/quarto"
 	var ctr *dagger.Container
 
-	if image != nil {
-		ctr = c.Container().From(*image)
+	if image != "" {
+		ctr = c.Container().From(image)
 	} else {
 		ctr = c.Container().From(defaultImageRepository)
 	}
