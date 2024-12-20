cff637584f76722a0e1bde708eadb9f4f08115c8
softwaredevelop <61334390+softwaredevelop@users.noreply.github.com>
2024-12-20 14:44:00 +0100

    refactor: Refactor editorconfig module

--- editorconfig/main.go
+++ editorconfig/main.go
@@ -17,33 +17,53 @@ import (
 	"dagger/editorconfig/internal/dagger"
 )
 
+const (
+	defaultImageRepository = "mstruebing/editorconfig-checker:latest"
+)
+
 // Editorconfig is a module for checking editorconfig files.
-type Editorconfig struct{}
+type Editorconfig struct {
+	Image string
+	Ctr   *dagger.Container
+}
+
+// New creates a new instance of the Editorconfig struct
+func New(
+	// Custom image reference in "repository:tag" format to use as a base container.
+	// +optional
+	image string,
+) *Editorconfig {
+	return &Editorconfig{
+		Image: image,
+	}
+}
+
+// Container returns the underlying Dagger container
+func (m *Editorconfig) Container() *dagger.Container {
+	if m.Ctr != nil {
+		return m.Ctr
+	}
+
+	image := m.Image
+	if image == "" {
+		image = defaultImageRepository
+	}
+
+	m.Ctr = dag.Container().From(image)
+	return m.Ctr
+}
 
 // Check runs the editorconfig-checker command.
 func (m *Editorconfig) Check(
-	// source is an optional argument that specifies a directory.
+	// Source directory
 	source *dagger.Directory,
 	// excludeDirectoryPattern is an optional argument that specifies a pattern to exclude directories.
 	// +default=".git"
 	excludeDirectoryPattern string,
 ) *dagger.Container {
-	return base().
+
+	return m.Container().
 		WithMountedDirectory("/tmp", source.WithoutDirectory(excludeDirectoryPattern)).
 		WithWorkdir("/tmp").
-		WithExec([]string{"/editorconfig-checker"})
-}
-
-// base returns a container with the editorconfig-checker binary installed.
-func base() *dagger.Container {
-	install := dag.
-		Container().
-		From("golang:alpine").
-		WithExec([]string{
-			"go", "install",
-			"github.com/editorconfig-checker/editorconfig-checker/cmd/editorconfig-checker@latest",
-		})
-
-	return dag.Container().
-		WithFile("/", install.File("/go/bin/editorconfig-checker"))
+		WithExec([]string{"ec"})
 }
